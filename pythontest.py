c78e521abe02d7192884838c3d68103c1a1b1488
Althaf S <althaf.sabsara@gmail.com>
2023-10-31 23:22:49 +0530

    Testing palindrome function
    Unittesting of palindrome function has been done
    and the changes are commited

--- pythontest.py
+++ pythontest.py
@@ -1,5 +1,6 @@
 import unittest
 from pythonhw import panagram
+from pythonhw import palindrome
 
 class TestPanagram(unittest.TestCase):
     def testIsPanagram(self):
@@ -11,6 +12,20 @@ class TestPanagram(unittest.TestCase):
     def testNullInput(self):
         ret = panagram("")
         self.assertFalse(ret)
+        
+class TestPalindrome(unittest.TestCase):
+    def testIsPalindrome(self):
+        sentence = "abba"
+        self.assertTrue(palindrome(sentence))
+    def testIsNotPalindrome(self):
+        sentence = "abcde"
+        self.assertFalse(palindrome(sentence))
+    def testNullInput(self):
+        empty = palindrome("")
+        self.assertTrue(empty)
+    def testSingleInputPalindrome(self):
+        sentence = palindrome("a")
+        self.assertTrue(sentence)
         
 if __name__ == "__main__":
   unittest.main()
