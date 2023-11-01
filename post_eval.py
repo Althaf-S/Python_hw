a713ac7be0135d419a70116fd5b9147d993e565c
Althaf S <althaf.sabsara@gmail.com>
2023-11-01 17:26:37 +0530

    GREEN : resolution of failure of second testcase
    resolution of failure of testcase for addition of two operands

--- post_eval.py
+++ post_eval.py
@@ -1,4 +1,13 @@
 def evaluation(expression):
-   return int(expression)
-
+   stack = []
+   for i in expression:
+     if i in ["+"]:
+       operand1 = stack.pop()
+       operand2 = stack.pop()
+       if i == "+":
+         stack.append(operand1 + operand2)
+     else:
+        stack.append(int(i))
+   
+   return stack.pop()
   
