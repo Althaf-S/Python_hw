a12516f1e0ba56bded2cb7d52a7d21c1e8a9e753
Althaf S <althaf.sabsara@gmail.com>
2023-11-02 14:09:02 +0530

    Edited the file for various errors

--- post_eval.py
+++ post_eval.py
@@ -1,7 +1,12 @@
 def evaluation(expression):
    stack = []
+   valid = "123456789+-*/"
    for i in expression:
-     if i in ["+","-","*","/"]:
+     if i not in valid:
+       return False
+     elif i in ["+","-","*","/"] and len(stack) < 2:
+       return False
+     elif i in ["+","-","*","/"]:
        operand1 = stack.pop()
        operand2 = stack.pop()
        if i == "+":
@@ -12,8 +17,16 @@ def evaluation(expression):
          stack.append(operand2 * operand1)
        elif i == "/":
          stack.append(operand2 // operand1)
+       elif i == "%":
+         stack.append(operand2 % operand1)
      else:
         stack.append(int(i))
    
    return stack.pop()
+
+def main():
+  evaluation(expressions)
   
+  
+if __name__ == "__main__":
+   main()
--- test_post.py
+++ test_post.py
@@ -14,4 +14,10 @@ def test_mul_two_operand():
   
 def test_div_two_operand():
   assert evaluation("62/") == 3
+  
+def test_one_operand_and_operator_only():
+  assert not evaluation("6+")
+  
+def test_symbol():
+  assert not evaluation("ab+")
    
